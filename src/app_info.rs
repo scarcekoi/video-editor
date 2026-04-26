82d0367e376e53cc3e495dece5044abb7991c656
Scarce Koi <scarcekoi@gmail.com>
2026-04-26 17:02:34 -0400

    feat: init

---
+++ src/app_info.rs
@@ -0,0 +1,7 @@
+pub const NAME: &str = "Video Editor";
+pub const ID: &str = "video-editor";
+
+pub const DESCRIPTION: &str = "A beautiful video editor built with Rust and performance in mind.";
+pub const AUTHOR: &str = "Scarce Koi";
+pub const LICENSE: &str = "GPL-3.0";
+pub const HOMEPAGE: &str = "https://github.com/scarcekoi/video-editor";
